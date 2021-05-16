6b650e4c3ab4e10c1f4e8764f650ae8f529b9c3c
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-05-16 22:04:49 +0300

    Форматирование числовых данных.

--- Chapter_02_Main_constructions/BasicConsoleIO/Program.cs
+++ Chapter_02_Main_constructions/BasicConsoleIO/Program.cs
@@ -8,7 +8,44 @@ namespace BasicConsoleIO
         {
             Console.WriteLine("***** Basic Console I/O");
             GetUserDate();
+
+            // Отдельный заполнитель допускается повторять внутри заданной строки.
+            Console.WriteLine("{0}, Number {0}, Number {0}", 9);
+            // Можно помещать каждый заполнитель в любую позицию внутри строкового литерала.
+            Console.WriteLine("{1} {0} {2}", 10, 20, 30);
+
+            FormatNumericalData();
+
+            DisplayMessage();
+
             Console.ReadLine();
+        }
+
+        private static void DisplayMessage()
+        {
+            // Использование string.Format() для форматирования строкового литереала.
+            string userMessage = string.Format("100000 in hex is {0:x}", 100000);
+
+            // Для этого кода требуется ссылка на PresentationFramework.dll
+            //System.Windows.MessageBox.Show(userMessage);
+        }
+
+        /// <summary>
+        /// Демонстрация применения некоторых дескрипторов формата.
+        /// </summary>
+        private static void FormatNumericalData()
+        {
+            Console.WriteLine("The value 99999 in various formats:");
+            Console.WriteLine("c format: {0:c}", 99999);
+            Console.WriteLine("d9 format: {0:d9}", 99999);
+            Console.WriteLine("f3 format: {0:f3}", 99999);
+            Console.WriteLine("n format: {0:n}", 99999);
+            // Использование для символа шестнадцаричного формата
+            // верхнего или нижнего регистра определяет регистр отображаемых символов.
+            Console.WriteLine("E format: {0:E}", 99999);
+            Console.WriteLine("e format: {0:e}", 99999);
+            Console.WriteLine("X format: {0:X}", 99999);
+            Console.WriteLine("x format: {0:x}", 99999);
         }
 
         private static void GetUserDate()
