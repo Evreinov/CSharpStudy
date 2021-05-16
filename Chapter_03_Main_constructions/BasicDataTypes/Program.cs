479b14ff95b9d18b096a2bc610d4179d338ddbbe
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-05-16 22:27:11 +0300

    Объявление и инициализация переменных.

--- Chapter_03_Main_constructions/BasicDataTypes/Program.cs
+++ Chapter_03_Main_constructions/BasicDataTypes/Program.cs
@@ -6,7 +6,35 @@ namespace BasicDataTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LocalVarDeclarations();
+            DefaultDeclarations();
+        }
+
+        private static void DefaultDeclarations()
+        {
+            Console.WriteLine("=> Default Declarations:");
+            int myInt = default;
+        }
+
+        private static void LocalVarDeclarations()
+        {
+            Console.WriteLine("=> Data Declarations:");
+            // Локальные переменные объявляются так:
+            // типДанных имяПеременной;
+
+            // Локальные переменные объявляются и инициализируются так:
+            // типДанных имяПеременной = начальноеЗначение;
+            int myInt = 0;
+            // Объявлять и присваивать можно также в двух отдельных строках.
+            string myString;
+            myString = "This is my character data";
+            // Объявить три перменных типа bool в одной строке.
+            bool b1 = true, b2 = false, b3 = b1;
+            // Использовать тип данных System.Boolean для объявления булевской переменной.
+            System.Boolean b4 = false;
+
+            Console.WriteLine("Your data: {0}, {1}, {2}, {3}, {4}, {5}", myInt, myString, b1, b2, b3, b4);
+            Console.WriteLine();
         }
     }
 }
