ce5de6bec98795cf26f0e90dfb04baa2e7a84f09
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-05-22 18:50:32 +0300

    Функциональность различных типов.

--- Chapter_03_Main_constructions/BasicDataTypes/Program.cs
+++ Chapter_03_Main_constructions/BasicDataTypes/Program.cs
@@ -10,6 +10,9 @@ namespace BasicDataTypes
             DefaultDeclarations();
             NewingDataTypes();
             ObjectFunctionality();
+            DataTypeFunctionality();
+            BooleanFunctionality();
+            CharFunctionality();
         }
 
         static void ObjectFunctionality()
@@ -21,6 +24,40 @@ namespace BasicDataTypes
             Console.WriteLine("12.Equals(23) = {0}", 12.Equals(23));
             Console.WriteLine("12.ToString() = {0}", 12.ToString());
             Console.WriteLine("12.GetType() = {0}", 12.GetType());
+            Console.WriteLine();
+        }
+
+        static void DataTypeFunctionality()
+        {
+            Console.WriteLine("=> Data type Functionality");
+            Console.WriteLine("Max of int: {0}", int.MaxValue);
+            Console.WriteLine("Min of int: {0}", int.MinValue);
+            Console.WriteLine("Max of double: {0}", double.MaxValue);
+            Console.WriteLine("Min of double: {0}", double.MinValue);
+            Console.WriteLine("double.Epsilon: {0}", double.Epsilon);
+            Console.WriteLine("double.PositiveInfinity: {0}", double.PositiveInfinity);
+            Console.WriteLine("double.NegativeInfinity: {0}", double.NegativeInfinity);
+            Console.WriteLine();
+        }
+
+        static void BooleanFunctionality()
+        {
+            Console.WriteLine("=> Boolean type Functionality");
+            Console.WriteLine("bool.FalseString: {0}", bool.FalseString);
+            Console.WriteLine("bool.TrueString: {0}", bool.TrueString);
+            Console.WriteLine();
+        }
+
+        static void CharFunctionality()
+        {
+            Console.WriteLine("=> char type Functionality");
+            char myChar = 'a';
+            Console.WriteLine("char.IsDigit('a'): {0}", char.IsDigit(myChar));
+            Console.WriteLine("char.IsLetter('a'): {0}", char.IsLetter(myChar));
+            Console.WriteLine("char.IsWhiteSpace('Hello There', 5): {0}", char.IsWhiteSpace("Hello There", 5));
+            Console.WriteLine("char.IsWhiteSpace('Hello There, 6'): {0}", char.IsWhiteSpace("Hello There", 6));
+            Console.WriteLine("char.IsPunctuation('?'): {0}", char.IsPunctuation('?'));
+            Console.WriteLine();
         }
 
         static void NewingDataTypes()
