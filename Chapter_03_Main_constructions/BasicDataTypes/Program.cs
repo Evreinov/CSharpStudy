7ca3577399340ea594067334e0cb341234b9df8c
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-05-22 18:32:57 +0300

    Внутренние типы данных и операция new, иерархия типов для типов данных.

--- Chapter_03_Main_constructions/BasicDataTypes/Program.cs
+++ Chapter_03_Main_constructions/BasicDataTypes/Program.cs
@@ -8,6 +8,29 @@ namespace BasicDataTypes
         {
             LocalVarDeclarations();
             DefaultDeclarations();
+            NewingDataTypes();
+            ObjectFunctionality();
+        }
+
+        static void ObjectFunctionality()
+        {
+            Console.WriteLine("=> System.Object Functionality:");
+            // Ключевое слово int языка C# - это в действительности сокращение для 
+            // типа System.Int32, который наследует от System.Object следующие члены:
+            Console.WriteLine("12.GetHashCode() = {0}", 12.GetHashCode());
+            Console.WriteLine("12.Equals(23) = {0}", 12.Equals(23));
+            Console.WriteLine("12.ToString() = {0}", 12.ToString());
+            Console.WriteLine("12.GetType() = {0}", 12.GetType());
+        }
+
+        static void NewingDataTypes()
+        {
+            Console.WriteLine("=> Using new to create variables:");
+            bool b = new bool(); // Устанавливается в false;
+            int i = new int(); // Устанавливается в 0.
+            double d = new double(); // Устанавливается в 0.
+            DateTime dt = new DateTime(); // // Устанавливается в 1/1/0001 12:00:00 AM
+            Console.WriteLine("{0}, {1}, {2}, {3}", b, i, d, dt);
         }
 
         private static void DefaultDeclarations()
