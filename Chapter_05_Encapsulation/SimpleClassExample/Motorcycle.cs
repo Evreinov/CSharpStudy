85219c713e4be71d7a6451b013871ed6412b564c
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-06-01 17:52:06 +0300

    Построение цепочки вызовов конструкторов с использованием this.

--- Chapter_05_Encapsulation/SimpleClassExample/Motorcycle.cs
+++ Chapter_05_Encapsulation/SimpleClassExample/Motorcycle.cs
@@ -6,23 +6,83 @@ using System.Threading.Tasks;
 
 namespace SimpleClassExample
 {
+    // Построение цепочки вызова конструкторов с использованием this.
     class Motorcycle
     {
         public int driverIntensity;
+        public string driverName;
 
-        // Новые члены для представления имени водителя.
-        public string name;
-        // Поле name будет пустой строкой, т.к. компилятор предполагает, что присваение идет к переменной внутри метода.
-        //public void SetDriverName(string name)
+        public Motorcycle() { }
+
+        //// Избыточная логика конструктора!
+        //// Проверка, значения intensity, встречается в каждом конструкторе, что приводит
+        //// к редактированию одного и того же кода в нескольких местах (избыточность).
+        //public Motorcycle(int intensity)
         //{
-        //    name = name;
+        //    if (intensity > 10)
+        //    {
+        //        intensity = 10;
+        //    }
+        //    driverIntensity = intensity;
         //}
-        // Присвоение к полю класса.
+
+        //public Motorcycle(int intensity, string name)
+        //{
+        //    if (intensity > 10)
+        //    {
+        //        intensity = 10;
+        //    }
+        //    driverIntensity = intensity;
+        //    driverName = name;
+        //}
+
+        // ==========================================================================================
+
+        // Другая избыточность, постоянно необходимо в каждом конструкторе
+        // вызывать метод SetIntensity(int intensity)
+        //public Motorcycle(int intensity)
+        //{
+        //    SetIntensity(intensity);
+        //}
+
+        //public Motorcycle(int intensity, string name)
+        //{
+        //    SetIntensity(intensity);
+        //    driverName = name;
+        //}
+
+        //public void SetIntensity(int intensity)
+        //{
+        //    if (intensity > 10)
+        //    {
+        //        intensity = 10;
+        //    }
+        //    driverIntensity = intensity;
+        //}
+
+        // ==========================================================================================
+
+        // Более совершенный подход связанный с построением цепочки вызовов конструкторов с использованием this
+        // Связывание конструкторов в цепочку.
+        public Motorcycle(int intensity) : this (intensity, "") { }
+
+        public Motorcycle(string name) : this (0, name) { }
+
+        // Это главный конструктор, выполняющий всю реальную работу.
+        public Motorcycle(int intensity, string name)
+        {
+            if (intensity > 10)
+            {
+                intensity = 10;
+            }
+            driverIntensity = intensity;
+            driverName = name;
+        }
+
+
         public void SetDriverName(string name)
         {
-           // Если поле класса и метода имеют разные названия, то ключевое слово this не обязательно.
-           // this.driverName = name;
-           this.name = name;
+            driverName = name;
         }
 
         public void PopAWheely()
@@ -31,18 +91,6 @@ namespace SimpleClassExample
             {
                 Console.WriteLine("Yeeeeeeee Haaaaaeewww!");
             }
-        }
-
-        // Вернуть стандартный конструктор, который будет 
-        // устанавливать все члены данных в стандартные значения.
-        // (т.к. если определен специальный конструктор, то стандартный конструктор
-        // молча удаляется из класса и перестает быть доступным, нужно стандартный конструктор явно переопределить)
-        public Motorcycle() { }
-
-        // Специальный конструктор.
-        public Motorcycle(int intensity)
-        {
-            driverIntensity = intensity;
         }
     }
 }
