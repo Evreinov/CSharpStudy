7a68a183736a5e59fc92a2967f761e02450efb36
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-06-01 18:10:09 +0300

    Конструктор с необязательными и наименованными аргументами.

--- Chapter_05_Encapsulation/SimpleClassExample/Motorcycle.cs
+++ Chapter_05_Encapsulation/SimpleClassExample/Motorcycle.cs
@@ -67,20 +67,34 @@ namespace SimpleClassExample
 
         // Более совершенный подход связанный с построением цепочки вызовов конструкторов с использованием this
         // Связывание конструкторов в цепочку.
-        public Motorcycle(int intensity) : this (intensity, "") 
-        {
-            Console.WriteLine("In ctor taking an int"); // Внутри конструктора, принимающего int.
-        }
+        //public Motorcycle(int intensity) : this (intensity, "") 
+        //{
+        //    Console.WriteLine("In ctor taking an int"); // Внутри конструктора, принимающего int.
+        //}
 
-        public Motorcycle(string name) : this(0, name)
-        {
-            Console.WriteLine("In ctor taking an string"); // Внутри конструктора, принимающего string.
-        }
+        //public Motorcycle(string name) : this(0, name)
+        //{
+        //    Console.WriteLine("In ctor taking an string"); // Внутри конструктора, принимающего string.
+        //}
 
-        // Это главный конструктор, выполняющий всю реальную работу.
-        public Motorcycle(int intensity, string name)
+        //// Это главный конструктор, выполняющий всю реальную работу.
+        //public Motorcycle(int intensity, string name)
+        //{
+        //    Console.WriteLine("In master ctor"); // Внутри главного конструктораю.
+        //    if (intensity > 10)
+        //    {
+        //        intensity = 10;
+        //    }
+        //    driverIntensity = intensity;
+        //    driverName = name;
+        //}
+
+        // ==========================================================================================
+
+        // Конструктор начиная с версии 4.0 с помощью необязательных аргументов.
+        // Единственный контсруктор, использующий необязательные аргументы.
+        public Motorcycle(int intensity = 0, string name = "")
         {
-            Console.WriteLine("In master ctor"); // Внутри главного конструктораю.
             if (intensity > 10)
             {
                 intensity = 10;
--- Chapter_05_Encapsulation/SimpleClassExample/Program.cs
+++ Chapter_05_Encapsulation/SimpleClassExample/Program.cs
@@ -60,6 +60,21 @@ namespace SimpleClassExample
             motorcycle.PopAWheely();
             Console.WriteLine("Rider name is {0}", motorcycle.driverName);
             Console.ReadLine();
+            MakeSomeBikes();
+            Console.ReadLine();
+        }
+
+        static void MakeSomeBikes()
+        {
+            //driverName = "", driverIntensity = 0
+            Motorcycle m1 = new Motorcycle();
+            Console.WriteLine("Name= {0}, Intensity= {1}", m1.driverName, m1.driverIntensity);
+            //driverName = "Tiny", driverIntensity = 0
+            Motorcycle m2 = new Motorcycle(name: "Tiny");
+            Console.WriteLine("Name= {0}, Intensity= {1}", m2.driverName, m2.driverIntensity);
+            //driverName = "", driverIntensity = 7
+            Motorcycle m3 = new Motorcycle(7);
+            Console.WriteLine("Name= {0}, Intensity= {1}", m3.driverName, m3.driverIntensity);
         }
     }
 }
