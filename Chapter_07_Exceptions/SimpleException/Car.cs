b45fc6e68fe316a30b124b9d734b9e51d8d8ef66
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-06-16 17:37:20 +0300

    Простейши пример генерации общего исключения.

---
+++ Chapter_07_Exceptions/SimpleException/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SimpleException
+{
+    class Car
+    {
+        // Константа для представления максимальной скорости.
+        public const int MaxSpeed = 100;
+
+        // Свойства автомобиля.
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        // Не вышел ли автомобиль из строя.
+        private bool carIsDead;
+        // В автомобиле имеется радиоприемник.
+        private Radio theMusicBox = new Radio();
+
+        // Конструкторы
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Делегировать запрос внутреннему объекту.
+            theMusicBox.TurnOn(state);
+        }
+
+        // Проверить, не перегрелся ли автомобиль.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    // Вывод ошибки с помощью текстового сообщения.
+                    //Console.WriteLine("{0} has overheated!", PetName);
+                    
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                    
+                    // Использование ключевого слова throw для генерации исключения.
+                    //throw new Exception($"{PetName} has overheated!");
+
+                    // Мы хотим обращаться к свойству HelpLink, поэтому необходимо
+                    // создать локальную переменную перед генерацией объекта Exception.
+                    Exception ex = new Exception($"{PetName} has overheated!");
+                    ex.HelpLink = "http://www.CarsRUs.com";
+
+                    // Предоставить специальные данные, касающиеся ошибки.
+                    ex.Data.Add("TimeStamp", $"The car exploded at {DateTime.Now}");
+                    ex.Data.Add("Cause", "You have a lead foot.");
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ Chapter_07_Exceptions/SimpleException/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple Exception Example *****");
+            Console.WriteLine("=> Creating a car and stepping on it!");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+
+            // Вариант без обработки исключения.
+            //for (int i = 0; i < 10; i++)
+            //    myCar.Accelerate(10);
+
+            // Разогнаться до скорости, превышающей максимальный
+            // предел автомобиля, с целью выдачи исключения.
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                    myCar.Accelerate(10);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n*** Error! ***");                                          // ошибка
+                // Свойство TargetSite в действительности возвращает объект MethodBase.
+                Console.WriteLine("Method: {0}", e.TargetSite);                                 // метод
+                Console.WriteLine("Class defining member: {0}", e.TargetSite.DeclaringType);    // класс, определяющий член
+                Console.WriteLine("Member type: {0}", e.TargetSite.MemberType);                 // тип члена.
+                Console.WriteLine("Message: {0}", e.Message);                                   // сообщение
+                Console.WriteLine("Source: {0}", e.Source);                                     // источник       
+                Console.WriteLine("Stack: {0}", e.StackTrace);                                  // стек вызова
+                Console.WriteLine("Help Link: {0}", e.HelpLink);                                // URL-адрес
+                Console.WriteLine("\n-> Custom Data:");
+                foreach (DictionaryEntry de in e.Data)
+                    Console.WriteLine("-> {0}: {1}", de.Key, de.Value);
+            }
+            // Ошибка была обработана, продолжается выполнение следующего оператора.
+            Console.WriteLine("\n***** Out of exception logic *****");
+            Console.ReadLine();
+        }
+    }
+}
