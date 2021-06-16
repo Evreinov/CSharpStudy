2050b1200c6fb983ac830508b8bb6533d13b2096
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-06-17 02:02:36 +0300

    Множественный и внутренние исключения, повторная генерация исключений, блок finally, общий оператор catch, фильтры исключений (when).

---
+++ Chapter_07_Exceptions/ProcessMultipleExceptions/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ProcessMultipleExceptions
+{
+    class Car
+    {
+        // Константа для представления максимальной скорости.
+        public const int MaxSpeed = 100;
+
+        // Свойства автомобиля.
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        // Не вышел ли автомобиль из строя.
+        private bool carIsDead;
+        // В автомобиле имеется радиоприемник.
+        private Radio theMusicBox = new Radio();
+
+        // Конструкторы
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Делегировать запрос внутреннему объекту.
+            theMusicBox.TurnOn(state);
+        }
+
+        // Проверить, не перегрелся ли автомобиль.
+        public void Accelerate(int delta)
+        {
+            // Перед продолжением проверить аргумент на предмет допустимости.
+            if (delta < 0)
+                throw new ArgumentOutOfRangeException("delta", "Speed must be greater than zero!"); // Значение скорости должно быть больше нуля!
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {                
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                    CarIsDeadException ex = new CarIsDeadException($"{PetName} has overheated!",
+                        "You have a lead foot", DateTime.Now);
+                    ex.HelpLink = "http://www.CarsRUs.com";
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+
+}
