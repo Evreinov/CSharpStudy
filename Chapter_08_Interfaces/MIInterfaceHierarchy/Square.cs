9a74b47e74884230f800b353ceb2f824d80b3993
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-06-18 02:13:25 +0300

    Множественное наследование с помощью интерфейсных типов.

---
+++ Chapter_08_Interfaces/MIInterfaceHierarchy/Square.cs
@@ -0,0 +1,24 @@
+﻿namespace MIInterfaceHierarchy
+{
+    // Специфические реализации для каждого метода Draw().
+    class Square : IShape
+    {
+        // Использование явной реализации для устранения конфликта имен членов.
+        void IDrawable.Draw()
+        {
+            // Вывести на экран...
+        }
+
+        void IPrintable.Draw()
+        {
+            // Вывести на принтер...
+        }
+
+        int IShape.GetNumberOfSides() => 4;
+
+        void IPrintable.Print()
+        {
+            // Печатать...
+        }
+    }
+}
