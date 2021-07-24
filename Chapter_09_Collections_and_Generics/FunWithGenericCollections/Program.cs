0d16d028659bfe359d9e0a9c55eb03f8c8909d5b
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-07-24 08:15:20 +0300

    Работа с классоми Stack<T> и Queue<T>.

--- Chapter_09_Collections_and_Generics/FunWithGenericCollections/Program.cs
+++ Chapter_09_Collections_and_Generics/FunWithGenericCollections/Program.cs
@@ -12,7 +12,14 @@ namespace FunWithGenericCollections
         {
             UseGenericList();
             Console.WriteLine();
+            Console.ReadLine();
 
+            UseGenericStack();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            UseGenericQueue();
+            Console.WriteLine();
             Console.ReadLine();
         }
 
@@ -45,6 +52,73 @@ namespace FunWithGenericCollections
             {
                 Console.WriteLine("First Names: {0}", p.FirstName);
             }
+        }
+
+        /// <summary>
+        /// Работа с классом Stack<T>.
+        /// </summary>
+        static void UseGenericStack()
+        {
+            Stack<Person> stackOfPeople = new Stack<Person>();
+            stackOfPeople.Push(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            stackOfPeople.Push(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            stackOfPeople.Push(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+            // Просмотреть верхний элемент, вытолкнуть его и просмотреть снова.
+            Console.WriteLine("First person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+            Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+            Console.WriteLine("\nFirst person item is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+            try
+            {
+                Console.WriteLine("\n\nFirst person is: {0}", stackOfPeople.Peek());
+                Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("\nError! {0}", ex.Message); // Ошибка! Стек пуст.
+            }
+        }
+
+        /// <summary>
+        /// Работа с классом Queue<T>.
+        /// </summary>
+        static void UseGenericQueue()
+        {
+            // Создать очередь из трех человек.
+            Queue<Person> peopleQ = new Queue<Person>();
+            peopleQ.Enqueue(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            peopleQ.Enqueue(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            peopleQ.Enqueue(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+            // Заглянуть, кто первый в очереди.
+            Console.WriteLine("{0} is first in line!", peopleQ.Peek().FirstName);
+
+            // Удалить всех из очереди
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
+
+            // Попробовать извлучь кого-то из очереди снова.
+            try
+            {
+                GetCoffee(peopleQ.Dequeue());
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Error! {0}", e.Message); // Ошибка! Очередь пуста.
+            }
+        }
+
+        // Вспомогательный метод для демонстрации работы класса Queue<T>.
+        static void GetCoffee(Person p)
+        {
+            Console.WriteLine("{0} got coffee!", p.FirstName);
         }
     }
 }
