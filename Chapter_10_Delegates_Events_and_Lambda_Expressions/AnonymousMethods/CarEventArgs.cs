a32222e044aaafbd91f4af9d12ff5edec961414c
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-07-30 10:29:01 +0300

    Понятие анонимных методов C#.

---
+++ Chapter_10_Delegates_Events_and_Lambda_Expressions/AnonymousMethods/CarEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+    /// <summary>
+    /// Специальный аргумент событий.
+    /// </summary>
+    public class CarEventArgs : EventArgs
+    {
+        public readonly string msg;
+        public CarEventArgs (string message)
+        {
+            msg = message;
+        }
+    } 
+}
