2e9cb162629a9b3ec4ab3b3a016307341b4ac195
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-07-30 10:11:28 +0300

    Понятие событий и обобщенный делегат EventHandler<T>.

---
+++ Chapter_10_Delegates_Events_and_Lambda_Expressions/CarDelegatesNoEncapsulation/Car.cs
@@ -0,0 +1,36 @@
+﻿namespace CarDelegatesNoEncapsulation
+{
+    class Car
+    {
+        // Данные состояния.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+
+        // Исправлен ли автомобиль?
+        private bool carIsDead;
+
+        // Конструкторы класса.
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        public delegate void CarEngineHandler(string msgForCaller);
+        // Теперь этот класс public!
+        public CarEngineHandler listOfHandlers;
+
+        public void Accelerate(int delta)
+        {
+            // Если этот автомобиль сломан, то отправить сообщение об этом.
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                    listOfHandlers("Sorry, this car is dead...");
+            }
+        }
+    }
+}
