49bd9d13545dc6e44b02d98ef445bb71827954eb
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-08-01 20:53:42 +0300

    Понятие специальных преобразований типов.

---
+++ Chapter_11_Advanced_language_facilities/CustomConversions/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CustomConversions
+{
+    public struct Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        
+        public Rectangle(int w, int h) : this()
+        {
+            Width = w;
+            Height = h;
+        }
+        
+        public void Draw()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public override string ToString() => $"[Width = {Width}; Height = {Height}]";
+
+        public static implicit operator Rectangle (Square s)
+        {
+            Rectangle r = new Rectangle
+            {
+                Height = s.Lenght,
+                Width = s.Lenght * 2 // Предположим, что ширина нового квадрата будет равна (Lenghth x 2).
+            };
+            return r;
+        }
+    }
+}
---
+++ Chapter_11_Advanced_language_facilities/CustomConversions/Square.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CustomConversions
+{
+    public struct Square
+    {
+        public int Lenght { get; set; }
+
+        public Square(int l) : this()
+        {
+            Lenght = l;
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < Lenght; i++)
+            {
+                for (int j = 0; j < Lenght; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public override string ToString() => $"[Lenght = {Lenght}]";
+
+        //Rectangle можно явно преобразовать в Square.
+        public static explicit operator Square(Rectangle r)
+        {
+            Square s = new Square { Lenght = r.Height };
+            return s;
+        }
+
+        public static explicit operator Square (int sideLength)
+        {
+            Square newSq = new Square { Lenght = sideLength };
+            return newSq;
+        }
+
+        public static explicit operator int (Square s) => s.Lenght;
+    }
+}
