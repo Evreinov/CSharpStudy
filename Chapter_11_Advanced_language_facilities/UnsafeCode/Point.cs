bc7c6d371d1689307c78268eae9fbdc7061dfadb
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-08-02 19:46:17 +0300

    Работа с типами указателей.

---
+++ Chapter_11_Advanced_language_facilities/UnsafeCode/Point.cs
@@ -0,0 +1,16 @@
+﻿namespace UnsafeCode
+{
+    struct Point
+    {
+        public int x;
+        public int y;
+        public override string ToString() => $"({x}, {y})";
+    }
+
+    class PointRef
+    {
+        public int x;
+        public int y;
+        public override string ToString() => $"({x}, {y})";
+    }
+}
