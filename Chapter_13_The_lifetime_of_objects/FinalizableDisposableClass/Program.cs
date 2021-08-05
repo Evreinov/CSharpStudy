6433d0c3e0a99af326373e1876086e242a618c83
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-08-05 20:35:04 +0300

    Создание финализируемых и освобождаемых типов. Формализованный шаблон освобождения.

---
+++ Chapter_13_The_lifetime_of_objects/FinalizableDisposableClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FinalizableDisposableClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Dispose() / Destructor Combo Platter *****");
+
+            // Вызвать метод Dispose() вручную. Это не приведет к вызову финализатора.
+            MyResourceWrapper rw = new MyResourceWrapper();
+            rw.Dispose();
+
+            // Не вызывать метод Dispose(). Это приведет к вызову финализатора
+            // и выдаче звукового сигнала.
+            MyResourceWrapper rw2 = new MyResourceWrapper();
+        }
+    }
+}
