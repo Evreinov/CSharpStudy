ad4b7e96e6f1a963a3be24d753e550c0e49a4c47
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-08-06 17:54:05 +0300

    Определение специальных пространств имен.

---
+++ Chapter_14_Building_and_configuring_class_libraries/CustomNamespaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShapes;
+using My3DShapes;
+// Устранить неоднозначность с помощью специальных псевдонимов.
+using The3DHexagon = My3DShapes.Hexagon;
+
+namespace CustomNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Разрешение конфликвто имен с помощью заданных имен.
+            MyShapes.Hexagon h = new MyShapes.Hexagon();
+            MyShapes.Circle c = new MyShapes.Circle();
+            MyShapes.Square s = new MyShapes.Square();
+
+            // На самом деле здесь создается экземпляр класса My3DShapes.Hexagon.
+            The3DHexagon h2 = new The3DHexagon();
+        }
+    }
+}
