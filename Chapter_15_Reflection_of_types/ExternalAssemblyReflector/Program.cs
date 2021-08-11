b5801667bb928b939e4745a9b95a8d5fbd3ef887
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-08-11 17:58:25 +0300

    Динамическая загрузка сборок.

---
+++ Chapter_15_Reflection_of_types/ExternalAssemblyReflector/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+using System.IO; // Для определения FileNotFoundException.
+
+namespace ExternalAssemblyReflector
+{
+    class Program
+    {
+        static void DisplayTypesInAsm(Assembly asm)
+        {
+            Console.WriteLine("\n***** Type in Assembly *****");
+            Console.WriteLine("->{0}", asm.FullName);
+            Type[] types = asm.GetTypes();
+            foreach (Type t in types)
+                Console.WriteLine("Type: {0}", t);
+            Console.WriteLine("");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** External Assembly Viewer *****");
+            string asmName = "";
+            Assembly asm = null;
+            do
+            {
+                Console.WriteLine("\nEnter an assembly to evaluate"); // Предложить ввести имя сборки
+                Console.Write("or enter Q to quit: "); // или Q для завершения.
+                // Получить имя сборки.
+                asmName = Console.ReadLine();
+                // Пользователь желает завершить программу?
+                if (asmName.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+                // Попробовать загрузить сборку.
+                try
+                {
+                    asm = Assembly.LoadFrom(asmName);
+                    DisplayTypesInAsm(asm);
+                }
+                catch
+                {
+                    Console.WriteLine("Sorry, can't find assembly."); // Сборка не найдена.
+                }
+            } while (true);
+        }
+    }
+}
