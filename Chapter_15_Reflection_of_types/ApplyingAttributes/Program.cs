1fe213fe11e3c1e3f8a192590546ee912d25861b
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-08-12 09:34:14 +0300

    Роль атрибутов .NET.

---
+++ Chapter_15_Reflection_of_types/ApplyingAttributes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyingAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HorseAndBuggy mule = new HorseAndBuggy();
+        }
+    }
+
+    // Этот класс может быть сохранен на диске.
+    [Serializable]
+    public class Motorcycle
+    {
+        // Однако это поле сохраняться не будет.
+        [NonSerialized]
+        float weightOfCurrentPassengers;
+        // Эти поля остаются сериализируемымы.
+        bool hasRadioSystem;
+        bool hasHeadSet;
+        bool hasSissyBar;
+    }
+
+    //[Serializable, Obsolete("Use another vehicle!")]
+    // Или
+    //[SerializableAttribute]
+    //[ObsoleteAttribute("Use another vehicle!")]
+    // Или
+    [Serializable]
+    [Obsolete("Use another vehicle!")]
+    public class HorseAndBuggy { }
+}
