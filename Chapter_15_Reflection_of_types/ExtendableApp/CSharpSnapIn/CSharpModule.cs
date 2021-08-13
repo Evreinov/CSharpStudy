8c5e2c2bce178bda6ac8f29516ddabb06220f1b5
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-08-13 19:08:28 +0300

    Построение расширяемого приложения.

---
+++ Chapter_15_Reflection_of_types/ExtendableApp/CSharpSnapIn/CSharpModule.cs
@@ -0,0 +1,14 @@
+﻿using CommonSnappableTypes;
+using System;
+
+namespace CSharpSnapIn
+{
+    [CompanyInfoAttribute(CompanyName = "FooBar", CompanyUrl = "www.FooBar.com")]
+    public class CSharpModule : IAppFunctionality
+    {
+        void IAppFunctionality.DoIt()
+        {
+            Console.WriteLine("You have just used the С# snap-in!");
+        }
+    }
+}
