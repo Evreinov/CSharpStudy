7b6dae875bf03f1295552f834aa34c0eeafb60b4
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-08-13 17:21:59 +0300

    Рефлексия атрибутов с использованием раннего связывания.

---
+++ Chapter_15_Reflection_of_types/VehickeDescriptionAttributeReader/Program.cs
@@ -0,0 +1,29 @@
+﻿// Выполнение рефлексии атрибутов с использованием раннего связывания.
+using AttributedCarLibrary;
+using System;
+
+namespace VehickeDescriptionAttributeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Value of VehicleDescriptionAttribute *****\n");
+            ReflectOnAttributesUsingEarlyBinding();
+            Console.ReadLine();
+        }
+
+        private static void ReflectOnAttributesUsingEarlyBinding()
+        {
+            // Получить объект Type, представляющий тип Winnebago.
+            Type t = typeof(Winnebago);
+
+            // Получить все атрибуты Winnebago.
+            object[] customAtts = t.GetCustomAttributes(false);
+
+            // Вывести описание.
+            foreach (VehicleDescriptionAttribute v in customAtts)
+                Console.WriteLine("-> {0}\n", v.Description);
+        }
+    }
+}
