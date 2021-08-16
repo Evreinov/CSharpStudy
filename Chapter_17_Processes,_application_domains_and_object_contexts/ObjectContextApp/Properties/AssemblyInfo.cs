51fa02c2fc2c098f0f5a2642b378d98799ae3cb5
Дмитрий Евреинов <d.evreinov@outlook.com>
2021-08-16 20:40:17 +0300

    Контекстные границы объектов.

---
+++ Chapter_17_Processes,_application_domains_and_object_contexts/ObjectContextApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+// связанные со сборкой.
+[assembly: AssemblyTitle("ObjectContextApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ObjectContextApp")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Если для атрибута ComVisible задать значение FALSE, типы в этой сборке будут невидимыми 
+// для компонентов COM. Если в этой сборке необходимо получить доступ к типу из 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6f267e2d-90fe-4462-8001-e951a8ac7455")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
