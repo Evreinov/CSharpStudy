7d39748197707359024b59287dab95a87390d474
Дмитрий Евреинов <d.evreinov@outlook.com>
2022-05-26 03:28:07 +0300

    Использование вместо создания потоков пула потоков CLR.

---
+++ Chapter_19_Multithreading_parallel_and_asynchronous_programming/ThreadPoolApp/Printer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolApp
+{
+    public class Printer
+    {
+        private readonly object threadLock = new object();
+        public void PrintNumbers()
+        {
+            // Использовать в качестве маркера блокировки закрытый член object.
+            lock (threadLock)
+            {
+                // Вывести информацию о потоке.
+                Console.WriteLine("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
+
+                // Вывести числа.
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    // Приостановить поток на случайный период времени.
+                    Random r = new Random();
+                    Thread.Sleep(1000 * r.Next(5));
+                    Console.Write("{0}, ", i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Chapter_19_Multithreading_parallel_and_asynchronous_programming/ThreadPoolApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with the CLR Thread Pool *****\n");
+
+            Console.WriteLine("Main thread started. ThreadID={0}", Thread.CurrentThread.ManagedThreadId);
+
+            Printer p = new Printer();
+
+            WaitCallback workItem = new WaitCallback(PrinterTheNumbers);
+
+            // Поставить в очередь метод 10 раз.
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(workItem, p);
+            }
+
+            Console.WriteLine("All task queued");
+
+            Console.ReadLine();
+        }
+
+        private static void PrinterTheNumbers(object state)
+        {
+            Printer task = (Printer)state;
+            task.PrintNumbers();
+        }
+    }
+}
