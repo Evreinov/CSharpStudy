f85ad99fb28a2139237cb6d21acf3b6b373894ec
Дмитрий Евреинов <d.evreinov@outlook.com>
2022-05-29 19:29:09 +0300

    Параллелизм с помощью класса Parallel. Запросы Parallel LINQ (PLINQ).

---
+++ Chapter_19_Multithreading_parallel_and_asynchronous_programming/MyEbookReader/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEbookReader
+{
+    internal static class Program
+    {
+        private static string _theEBook;
+
+        private static void Main(string[] args)
+        {
+            GetBook();
+            Console.ReadLine();
+        }
+
+        private static void GetBook()
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadStringCompleted += (s, eArgs) =>
+            {
+                _theEBook = eArgs.Result;
+                Console.WriteLine("Download complete.");
+                GetStats();
+            };
+            // Загрузить электронную книгу Чарльза Диккенса "A Tale of Two Cities".
+            // Может понадобиться двукрастное выполнение этого кода, если ранее вы 
+            // не посещали данный сайт, поскольку при первом его посещение появляется
+            // окно с сообщением, предотвращающее нормальное выполнение кода.
+            wc.DownloadStringAsync(new Uri("https://www.gutenberg.org/files/2600/2600-0.txt"));
+        }
+
+        private static void GetStats()
+        {
+            // Получить слова из электронной книги.
+            string[] words = _theEBook.Split(new char[] { ' ', '\u000A', ',', '.', ';', ':', '-', '?', '/' },
+                StringSplitOptions.RemoveEmptyEntries);
+            string[] tenMostCommon = null;
+            string longestWord = string.Empty;
+
+            Parallel.Invoke(
+                () =>
+                {
+                    // Найти 10 наиболее часто встречающихся слов.
+                    tenMostCommon = FindTenMostCommon(words);
+                },
+                () =>
+                {
+                    // Получить самое длинное слово.
+                    longestWord = FindLongestWord(words);
+                });
+
+            // Когда все задачи завершиины, посроить строку,
+            // показывающую всю статистику в окне сообщений.
+            StringBuilder bookStats = new StringBuilder("Ten Most Common Words are:\n");
+            foreach (string s in tenMostCommon)
+            {
+                bookStats.AppendLine(s);
+            }
+
+            bookStats.AppendFormat("Longest word is: {0}", longestWord); // Самое длинное слово.
+
+            bookStats.AppendLine();
+
+            Console.WriteLine(bookStats.ToString(), "Book info"); // Информация о книге.
+        }
+
+        private static string FindLongestWord(string[] words)
+        {
+            return (from w in words
+                    orderby w.Length descending
+                    select w).FirstOrDefault();
+        }
+
+        private static string[] FindTenMostCommon(string[] words)
+        {
+            var frequencyOrder = from word in words
+                                 where word.Length > 6
+                                 group word by word
+                into g
+                                 orderby g.Count() descending
+                                 select g.Key;
+            string[] commonWords = (frequencyOrder.Take(10)).ToArray();
+            return commonWords;
+        }
+    }
+}
---
+++ Chapter_19_Multithreading_parallel_and_asynchronous_programming/PLINQDataProcessingWithCancellation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PLINQDataProcessingWithCancellation
+{
+    internal class Program
+    {
+        private static CancellationTokenSource cancelToken = new CancellationTokenSource();
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Start any key to start processing"); // Нажмите любую клавишу для начала обработки.
+                Console.ReadKey();
+                Console.WriteLine("Processing");
+                Task.Factory.StartNew(() => ProcessIntData());
+                Console.WriteLine("Enter Q tu quit: "); // Введите Q  для выхода:
+                string answer = Console.ReadLine();
+                // Желает ли пользователь выйти?
+                if (answer.Equals("Q", StringComparison.OrdinalIgnoreCase))
+                {
+                    cancelToken.Cancel();
+                    break;
+                }
+            } while (true);
+            Console.ReadLine();
+        }
+
+        static void ProcessIntData()
+        {
+            // Получить очень большой массив целых чисел.
+            int[] source = Enumerable.Range(1, 10_000_000).ToArray();
+
+            // Найти числа, для которых истинно условие num % 3 == 0,
+            // и возвратить их в убывающем порядке.
+            int[] modThreeIsZero = null;
+
+            try
+            {
+                modThreeIsZero = (from num in source.AsParallel().WithCancellation(cancelToken.Token)
+                    where num % 3 == 0
+                    orderby num descending
+                    select num).ToArray();
+                Console.WriteLine();
+                // Вывести количесвто найденных чисел.
+                Console.WriteLine($"Found {modThreeIsZero.Length} numbers that match query!");
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
