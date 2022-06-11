2ef230da8becd8881d980598883ddf92547a3c67
Дмитрий Евреинов <d.evreinov@outlook.com>
2022-06-11 09:41:04 +0300

    Работа с типом Файл.

---
+++ Chapter_20_File_IO_and_Object_Serialization/SimpleFileIO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleFileIO
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Работа с типом File. (Аналогичный класс есть, FileInfo;
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Simple I/O with the File Type *****\n");
+            string[] myTasks = {"Fix bathroom sink", "Call Dave", "Call Mom and Dad", "Play Xbox One"};
+
+            // Записать все данные в файл на диске С:.
+            File.WriteAllLines(@"tasks.txt", myTasks);
+
+            // Прочитать все данные и вывести на консоль.
+            foreach (string task in File.ReadAllLines(@"tasks.txt"))
+            {
+                Console.WriteLine("TODO: {0}", task);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
